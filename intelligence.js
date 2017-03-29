5a05eedb7e6e4522a597ab8569cf94a237d4b374
Unknown <dlomugdang@mail.csuchico.edu>
2017-03-28 21:27:53 -0700

    Working problems

--- intelligence.js
+++ intelligence.js
@@ -3,16 +3,7 @@ function solutionOne(x,y){
   return (x-y).toString() + (x+y).toString() + (x*y).toString();
 }
 
-function solutionTwo(x,y){
-  return (x-y).toString() + (x+y).toString() + (x*y).toString();
-}
-
 function problemOne(){
-  var valEnter = document.getElementById("userNum").value;
-  alert(valEnter);
-}
-
-function problemTwo(){
   var valEnter = document.getElementById("userNum").value;
   alert(valEnter);
 }
@@ -20,18 +11,14 @@ function problemTwo(){
 function validate(){
   var num = document.getElementById("userNum").value;
   //var valEnter = document.getElementById("userNum").value
-  if(num == solutionOne(8, 3)){
+  if(num == solutionOne(24, 7)){
     //alert(valEnter + " Correct");
     //document.getElementById("userVal").innerHTML="Correct";
     document.getElementById("userVal").classList.add("has-success");
     document.getElementById("userVal").classList.remove("has-error");
   }
-  /*else if(num == solutionOne(10,6)){
-    document.getElementById("userVal").classList.add("has-success");
-    document.getElementById("userVal").classList.remove("has-error");
-  }*/
   else{
-    document.getElementById("userError").innerHTML="Incorrect!";
+    //document.getElementById("userError").innerHTML="Incorrect!";
     document.getElementById("userError").classList.remove("hidden-message");
     document.getElementById("userError").classList.add("shown-message");
     document.getElementById("userVal").classList.add("has-error");
@@ -39,14 +26,39 @@ function validate(){
   }
 }
 
+function solutionTwo(string){
+  return sentence.toString();
+}
+
+function problemTwo(){
+  var valEnter = document.getElementById("userNum").value;
+  alert(valEnter);
+}
 function validate2(){
-  var num = document.getElementById("userNum").value;
-  if(num == solutionTwo(10,6)){
+  var sentence = document.getElementById("userNum").value;
+  if(sentence == "100 cents in a dollar"){
     document.getElementById("userVal").classList.add("has-success");
     document.getElementById("userVal").classList.remove("has-error");
   }
   else{
-    document.getElementById("userError").innerHTML="Incorrect!";
+    document.getElementById("userError").classList.remove("hidden-message");
+    document.getElementById("userError").classList.add("shown-message");
+    document.getElementById("userVal").classList.add("has-error");
+    document.getElementById("userVal").classList.remove("has-success")
+  }
+}
+
+function solutionThree(string){
+  return (bin).toString(2) + (bin).toString(2);
+}
+
+function validate3(){
+  var bin = document.getElementById("userNum").value;
+  if(bin == "01010110 01001101"){
+    document.getElementById("userVal").classList.add("has-success");
+    document.getElementById("userVal").classList.remove("has-error");
+  }
+  else{
     document.getElementById("userError").classList.remove("hidden-message");
     document.getElementById("userError").classList.add("shown-message");
     document.getElementById("userVal").classList.add("has-error");
